90ff19c597e18a64ad3126fa4d079a2266948daa
Himanshu Bhirud <116067299+W47K3RJO1N@users.noreply.github.com>
2026-02-12 22:51:58 +0530

    Add files via upload

---
+++ Basics/oddoreven.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a ;
+    cin >> a;
+
+    if (a % 2 == 0) {
+        cout << "The Entered number is Even";
+    }
+    else {
+        cout << "The Entered number is Odd";
+    }
+    return 0;
+}
