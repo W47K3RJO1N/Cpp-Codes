c09ac4a15f14f969e960759b8f04e80b41d1a73c
Himanshu Bhirud <116067299+W47K3RJO1N@users.noreply.github.com>
2026-03-01 23:33:25 +0530

    Add files via upload

---
+++ Arrays/swapreverse.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+    int n = sizeof(arr) / 4;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+   int  i = 0 , j = n - 1;
+while (i < j)  
+{
+    swap(arr[i],arr[j]);
+    i++;
+    j--;
+}
+
+for ( int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+}
+} 
