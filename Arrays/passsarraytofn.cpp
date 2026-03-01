93fb57deb2b86f589edfe630d586ef404e1d679a
Himanshu Bhirud <116067299+W47K3RJO1N@users.noreply.github.com>
2026-03-01 21:52:38 +0530

    Add files via upload

---
+++ Arrays/passsarraytofn.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void change(int x) {
+    x = 8;
+}
+int main() {
+    int x = 5;
+    cout << x << endl;
+
+    change(x);
+    cout << x << endl;
+}
